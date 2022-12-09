158f07fcdce9b705770798581abe0efb929cb57d
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-12-09 16:56:32 +0530

    Import newly created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import Todos from './components/Todos';
+import AddTask from './components/AddTask';
 
 function App() {
   return (
-    <div className="flex justify-center items-center bg-black text-white">
-      Hello, World!
+    <div className='bg-white max-w-full max-h-full m-0 p-0'>
+      <div className='flex justify-center items-center'>
+        <Popup trigger={
+          <button type='button' className='bg-gradient-to-r from-green-600 to-indigo-500 rounded-3xl border-solid border-green-400 text-white p-2 m-4'>+ Add Task</button>
+        }>
+          <AddTask />
+        </Popup>
+      </div>
+      <div className='flex flex-col justify-start'>
+        <Todos task='Task 1'/>
+        <Todos task='Task 2'/>
+        <Todos task='Task 3'/>
+      </div>
     </div>
   );
 }
