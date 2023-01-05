17d3342780fd5716fa9e2f0006751484d7c0b316
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-01-05 13:53:37 +0530

    :lipstick: Change border width

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,7 @@
 function Todos(props: any) {
     
     return (
-        <div className="border-2 border-black max-w-xl mt-2 ml-4 mr-4 p-2">
+        <div className="border border-black max-w-xl mt-2 ml-4 mr-4 p-2">
             <label>
                 <input type='checkbox' name="isDone"/>
                 {props.task}
