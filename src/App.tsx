50cbb185d74fbeeee80c49a80ec8562b2fbb9d38
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-01-13 12:06:04 +0530

    :sparkles: Introduce add task feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
-import Todos from './components/Todos';
-import AddTask from './components/AddTask';
 
-function App() {
-  return (
-    <div className='bg-white max-w-full max-h-full m-0 p-0'>
-      <div className='flex justify-center items-center'>
-        <Popup trigger={
-          <button type='button' className='bg-gradient-to-r from-green-600 to-indigo-500 rounded-3xl border-solid border-green-400 text-white p-2 m-4'>+ Add Task</button>
-        }>
-          <AddTask />
-        </Popup>
+class App extends React.Component <any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      key: 0,
+      task : {
+        id: '',
+        title: ''
+      },
+      tasks: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleChange(e: any) {
+    e.preventDefault();
+    this.setState({
+      task: {
+        id: this.state.key,
+        title: e.target.value
+      }
+    }) 
+  }
+
+  handleClick(e: any) {
+    e.preventDefault();
+    if(this.state.task.title !== '') {
+      this.setState({
+        tasks : [...this.state.tasks, this.state.task],
+        task: {
+          id: '',
+          title: ''
+        },
+        key: this.state.key + 1,
+      })
+    } 
+  }
+
+  render ()  {
+    return(
+      <div className='flex flex-col justify-center items-center'>
+        <div className='bg-blue-500 rounded-xl p-5 flex flex-row' >
+          <input className='border border-black p-4' type='text' value={ this.state.task.title } onChange={ this.handleChange } name='task' placeholder='Add task here' />
+          <button className='border border-white text-white bg-green-600 ml-4 p-4 font-bold rounded-full' type='button' onClick={ this.handleClick }>Add</button>
+        </div>
+        <div className='flex flex-col justify-start items-start border border-black rounded-xl p-5 mt-4 box-border'>
+          <ul className='w-96 overflow-auto'>
+            {this.state.tasks.map((task: any) => {
+              return <li key={ task.id }>{ task.title }</li>
+            })}
+          </ul>
+        </div>
       </div>
-      <div className='flex flex-col justify-start'>
-        <Todos task='Task 1'/>
-        <Todos task='Task 2'/>
-        <Todos task='Task 3'/>
-      </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
