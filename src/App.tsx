cf2db7953f18684eaf135547e42424c3c8c792bf
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-04-08 05:00:46 +0000

    :fire: Reset code to hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,10 @@
 import React from 'react';
 
 class App extends React.Component <any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      key: 0,
-      task : {
-        id: '',
-        title: ''
-      },
-      tasks: []
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleKeyEvent = this.handleKeyEvent.bind(this);
-  }
   
-  handleChange(e: any) {
-    e.preventDefault();
-    this.setState({
-      task: {
-        id: this.state.key,
-        title: e.target.value
-      }
-    }) 
-  }
-
-  handleClick() {
-    if(this.state.task.title !== '') {
-      this.setState({
-        tasks : [...this.state.tasks, this.state.task],
-        task: {
-          id: '',
-          title: ''
-        },
-        key: this.state.key + 1,
-      })
-    } 
-  }
-
-  handleKeyEvent(e: any){
-    if(e.key === 'Enter'){
-      this.handleClick();
-    }
-  }
-
   render ()  {
     return(
-      <div className='flex flex-col justify-center items-center'>
-        <div className='bg-blue-500 rounded-xl p-5 flex flex-row' >
-          <input className='border border-black p-4' type='text' value={ this.state.task.title } onKeyDown={ this.handleKeyEvent } onChange={ this.handleChange } name='task' placeholder='Add task here' />
-          <button className='border border-white text-white bg-green-600 ml-4 p-4 font-bold rounded-full' type='button' onClick={ this.handleClick }>Add</button>
-        </div>
-        <div className='flex flex-col justify-start items-start p-5 mt-4 box-border'>
-          <ul className='w-96 overflow-auto'>
-            {this.state.tasks.map((task: any) => {
-              return <li className='border box-border p-2 border-black rounded mt-2' key={ task.id }>{ task.title }</li>
-            })}
-          </ul>
-        </div>
-      </div>
+      <>Hello, world!</>
     )
   }
 }
