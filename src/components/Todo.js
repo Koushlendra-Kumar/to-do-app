80fd45e3d9b1519f2cb8a42f666847eebcf8759f
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-04-19 11:16:48 +0530

    :sparkles: Add keyboard accessibility

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+  const wasEditing = usePrevious(isEditing);
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
 
   function handleChange(e) {
     setNewName(e.target.value)
@@ -27,6 +38,7 @@ export default function Todo(props) {
           value={newName} 
           type='text'
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
       <div className='btn-group'>
@@ -60,6 +72,7 @@ export default function Todo(props) {
           type='button' 
           className='btn'
           onClick={() => setEditing(true)}
+          ref={editButtonRef}
         >
           Edit <span className='visually-hidden'>{props.name}</span></button>
         <button 
@@ -72,9 +85,17 @@ export default function Todo(props) {
     </div>
   );
 
-    return (
-        <li className='todo'>
-          {isEditing ? editingTemplate : viewTemplate}
-        </li>
-    );
+  useEffect(() => {
+    if(!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    } if(wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
+
+  return (
+      <li className='todo'>
+        {isEditing ? editingTemplate : viewTemplate}
+      </li>
+  );
 }
