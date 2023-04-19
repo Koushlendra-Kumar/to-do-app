3398a1c93ccc5d819a46825e021f9c4b4af2587f
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-04-19 10:26:39 +0530

    :sparkles: Add edit filter feature

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import Todo from './components/Todo';
 import FilterButton from './components/FilterButton';
 import Form from './components/Form';
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All');
 
   function addTask(name){
     const newTask = {id: `todo-${nanoid()}`, name, completed: false};
@@ -28,7 +37,19 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks?.map((task) => (
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if(task.id === id){
+        return {...task, name: newName};
+      }
+      return task;
+    })
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
       <Todo 
         id={task.id} 
         name={task.name} 
@@ -36,8 +57,18 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        editTask={editTask}
       />
     ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name===filter}
+      setFilter={setFilter}
+    />
+  ));
   
   const taskNoun = taskList.length === 1 ? 'task' : 'tasks';
   const headingText = `${taskList.length} ${taskNoun} remaining`;
@@ -47,9 +78,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className='filters btn-group stack-exception'>
-        <FilterButton type='all' pressed='true' />
-        <FilterButton type='active' pressed='false' />
-        <FilterButton type='completed' pressed='false' />
+        {filterList}
       </div>
       <h2 id='list-heading'>{headingText}</h2>
       <ul role='list' className='todo-list stack-large stack-exception' aria-labelledby='list-heading'>
